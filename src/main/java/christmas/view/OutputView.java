97fb98d27bec53ff81cbc37eb06cb267047502e0
maark1106 <mark1106@naver.com>
2023-11-09 19:52:51 +0900

    feat : 받을 혜택 안내문구 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void printWelcomeMessage(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+
+    public static void printBenefitPreviewMessage(){
+        System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
 }
