edbd84394debe36b011838147bd39d7b3e23bb3a
maark1106 <mark1106@naver.com>
2023-11-09 19:35:00 +0900

    feat : 주문할 메뉴 입력받는 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -14,4 +14,10 @@ public class InputView {
         validateVisitDate(visitDate);
         return Integer.parseInt(visitDate);
     }
+
+    public static String InputOrderMenus(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String orderMenus = Console.readLine();
+        return orderMenus;
+    }
 }
