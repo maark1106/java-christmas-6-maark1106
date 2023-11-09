f3e2886301599145f84dcae0474daaad9733525f
maark1106 <mark1106@naver.com>
2023-11-09 19:07:05 +0900

    docs : 기능 목록 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,6 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("프로그램 테스트u");
+        System.out.println("프로그램 테스트");
     }
 }
