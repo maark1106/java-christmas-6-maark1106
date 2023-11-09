06a970d088feccb3df08e404a33a83bb34b5534a
maark1106 <mark1106@naver.com>
2023-11-09 19:28:24 +0900

    feat : 입력 값이 정수인지 검증하는 기능 추가

--- src/main/java/christmas/util/InputValidator.java
+++ src/main/java/christmas/util/InputValidator.java
@@ -4,6 +4,15 @@ public class InputValidator {
 
     public static void validateVisitDate(String visitDate){
         inputEmptyException(visitDate);
+        inputNotIntegerTypeException(visitDate);
+    }
+
+    private static void inputNotIntegerTypeException(String visitDate) {
+        try {
+            Integer.parseInt(visitDate);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("입력값이 정수가 아닙니다.");
+        }
     }
 
     private static void inputEmptyException(String visitDate) {
