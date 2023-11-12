9a2dce7e3e3dd91050743178ed25b3229ef064d6
maark1106 <mark1106@naver.com>
2023-11-12 15:07:52 +0900

    test : 주문 메뉴 입력 예외 테스트

--- src/test/java/christmas/domain/MyOrderTest.java
+++ src/test/java/christmas/domain/MyOrderTest.java
@@ -3,9 +3,12 @@ package christmas.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import christmas.util.InputValidator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MyOrderTest {
 
@@ -39,5 +42,15 @@ class MyOrderTest {
         String menus = "해산물파스타-10,레드와인-8,초코케이크-3";
         assertThatThrownBy(() -> new MyOrder(menus))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 메뉴 입력 형식이 맞지 않으면 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "양송이수프 -1", "양송이수프- 2", "양송이수프-2 ",
+            "양송이수프 - 2", "양송이수프-1, 티본스테이크-3", "양송이수프-3, "})
+    void validateOrderMenusTest(String orderMenus) {
+        assertThatThrownBy(() -> InputValidator.validateOrderMenus(orderMenus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
--- src/test/java/christmas/util/InputValidatorTest.java
+++ src/test/java/christmas/util/InputValidatorTest.java
@@ -3,16 +3,31 @@ package christmas.util;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
+import christmas.domain.MyOrder;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
 
-    @DisplayName("정수가 아니면 예외 발생")
-    @Test
-    void inputNotIntegerTypeExceptionTest() {
-        assertThatThrownBy(() -> InputValidator.validateVisitDate("12a"))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("방문 날짜 입력 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "12a", "날짜", "11.1"})
+    void validateVisitDateTest(String visitDate) {
+        assertThatThrownBy(() -> InputValidator.validateVisitDate(visitDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("주문 메뉴 입력 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"양송이수프2", "양송이수프--2", "양송이수프*3,초코케이크-2",
+    "양송이수프","양송이수프3-","양송이수프-2,"})
+    void validOrderFormatTest(String orderMenus) {
+        assertThatThrownBy(() -> new MyOrder(orderMenus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
